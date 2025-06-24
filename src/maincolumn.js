b333b350155c64a073a9d7f0b1011b59e20c293f
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-06-24 20:44:06 +0530

    Save and delete button functionalities done

--- src/maincolumn.js
+++ src/maincolumn.js
@@ -16,14 +16,14 @@ export const createnewtaskbutton = ()=>{
     return {newtaskbutton}
 }
 
-
-
 // event listener for task button
 
 export const newtaskbuttonEL = (newtaskbutton)=>{
     newtaskbutton.addEventListener("click",()=>{
         const task = new todo();
         tasklisteners(task);
+        savebuttonlistener(task);
+        deletebuttonlistener(task);
     })
 }
 
@@ -37,10 +37,58 @@ export const tasklisteners = (task)=>{
         task.desc = e.target.value;
         console.log(task.desc);
     })
-
+    
     task.datefield.addEventListener("input",(e)=>{
         task.date = e.target.value;
         console.log(task.date);
     })
+}
 
+
+export const savebuttonlistener = (task)=>{
+
+    // console.log(task.savebutton);
+    task.savebutton.addEventListener("click",()=>{
+
+        task.taskcontainer.textContent = "";
+        task.taskcontainer.classList.remove("taskcontainer-style");
+        task.taskcontainer.classList.add("savedtask");
+
+        const savedtitle = document.createElement("p");
+        savedtitle.textContent = task.titlefield.value;
+        savedtitle.classList.add("savedtitle-style");
+        if ( savedtitle.textContent === ""){
+            savedtitle.textContent = "Title";
+        }
+        
+        const saveddesc = document.createElement("p");
+        saveddesc.classList.add("saveddesc-style")
+        saveddesc.textContent = task.descriptionbox.value;
+        if ( saveddesc.textContent === ""){
+            saveddesc.textContent = "Description";
+        }
+        const saveddate = document.createElement("p");
+        saveddate.textContent = task.datefield.value;
+        saveddate.classList.add("saveddate-style");
+        if ( saveddate.textContent === ""){
+            saveddate.textContent = "Date";
+        }
+        task.taskcontainer.append(savedtitle,saveddesc,saveddate);
+
+        const editbutton = document.createElement("button");
+        editbutton.textContent = "Edit";
+        editbutton.classList.add("editbutton-style");
+        task.taskcontainer.append(editbutton);
+    })
+}
+
+export const deletebuttonlistener = (task)=>{
+    task.deletebutton.addEventListener("click",()=>{
+        console.log('helo')
+        task.taskcontainer.remove();
+    })
+}
+
+export const editbuttonlistener = (task)=>{
+    
 }
