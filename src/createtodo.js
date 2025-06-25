8d8b3681a37bab83eaf05399da7f9f581a9fbd8f
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-06-25 07:02:32 +0530

    refactored the class for cleaner edit-save changes-handling

--- src/createtodo.js
+++ src/createtodo.js
@@ -1,12 +1,9 @@
 export class todo {
     constructor(){
+        this.createform();
+    }
 
-        console.log("welcome")    
-
-        this.title = "";
-        this.date = "";
-        this.desc = "";
-
+    createform(title,date ,desc){
 
         this.maindiv = document.getElementById("tasks");
         this.taskcontainer = document.createElement("div");
@@ -32,10 +29,16 @@ export class todo {
 
         this.titlesection.append(this.titlesectionleft,this.titlesectionright);
 
-
         this.titlefield = document.createElement("input");
         this.titlefield.type="text";
-        this.titlefield.placeholder = "Title";
+        if ( title === undefined){
+            this.titlefield.placeholder = "Title";
+        }
+        else{
+            this.titlefield.value = title;
+        }
+
+        console.log(title,"is title.")
         this.titlefield.classList.add("titlefield-style");
 
         this.titlesectionleft.append(this.titlefield);
@@ -45,6 +48,8 @@ export class todo {
 
         this.datefield = document.createElement("input");
         this.datefield.type = "date";
+        this.datefield.value = date;
+
         this.datefield.classList.add("datefield");
         this.titlesectionright.append(this.datelabel,this.datefield);
 
@@ -55,7 +60,13 @@ export class todo {
         //description section: 
         this.descriptionbox = document.createElement("textarea");
         this.descriptionbox.classList.add("descriptionbox-style");
-        this.descriptionbox.placeholder = "Description";
+        if (desc === undefined){
+            this.descriptionbox.placeholder = "Description";
+        }
+        else{
+            this.descriptionbox.value = desc;
+        }
+    
         this.descriptionsection.append(this.descriptionbox);
         this.taskcontainer.append(this.descriptionsection);
 
@@ -91,5 +102,6 @@ export class todo {
 
         this.controlsection.append(this.savebutton, this.deletebutton);
         this.taskcontainer.append(this.controlsection);
+        return this.taskcontainer;
     }
 }
