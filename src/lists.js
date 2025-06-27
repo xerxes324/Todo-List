3f243840af97604cfcf2b6212415d4376b6bbc20
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-06-27 17:52:16 +0530

    Created lists for handling sidebar functionality and ID tracking

---
+++ src/lists.js
@@ -0,0 +1,38 @@
+import { listtaskmap, active } from ".";
+export class initializelist {
+
+    constructor(){
+        this.createlist();
+        this.id = crypto.randomUUID();
+        listtaskmap.set(this.id,[]);
+    }
+
+    createlist(listinputvalue){
+        this.listwrapper = document.getElementById("lists");
+        this.listwrapper.classList.add("listcontainer-style")
+        this.listbox = document.createElement("div");
+        this.listbox.classList.add("newlist-style");
+        this.listinput = document.createElement("input");
+        this.listinput.type = "text";
+        if ( listinputvalue === undefined){
+            this.listinput.placeholder = "List";
+        }
+        else{
+            this.listinput.value = listinputvalue;
+        }
+        this.listinput.classList.add("liststyle");
+
+        this.savelistbtn = document.createElement("button");
+        this.savelistbtn.textContent = "Save";
+        this.savelistbtn.classList.add("listbutton-style");
+
+        this.deletelistbtn = document.createElement("button");
+        this.deletelistbtn.textContent = "Delete";
+        this.deletelistbtn.classList.add("listbutton-style");
+
+        this.listbox.append(this.listinput,this.savelistbtn,this.deletelistbtn);
+        this.listwrapper.append(this.listbox);
+
+        return this.listbox;
+    }
+}
