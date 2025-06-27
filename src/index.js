b1feec2e243f4e8791a8256c53a3c20ca48f9454
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-06-27 17:53:24 +0530

    Initialized new Map and a new array for tracking selected list

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import "./sidebar.css";
 import "./maincolumn.css"
 import {mylistheading,createhomebutton,createtodaybutton,createweekbutton,homebuttonEL,weekbuttonEL,todaybuttonEL,createlistbutton, newlistbuttonEL} from "./sidebar.js";
 import { tasksheading,createnewtaskbutton,newtaskbuttonEL} from "./maincolumn.js";
-// import {tasklisteners} from "./maincolumn.js";
-
+export const active = [];
+export const listtaskmap = new Map();
 
 mylistheading();
 const {homebutton} = createhomebutton();
